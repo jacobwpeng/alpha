d5fadab66652c639c84789bb6b4140e70cc0bbf0
Peng Wang <pw2191195@gmail.com>
2015-05-02 23:33:30 +0800

    增加HexDump打印二进制数据

---
+++ alpha/format.h
@@ -0,0 +1,22 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  format.h
+ *        Created:  05/02/15 23:01:52
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:  
+ *
+ * ==============================================================================
+ */
+
+#ifndef  __FORMAT_H__
+#define  __FORMAT_H__
+
+#include "slice.h"
+
+namespace alpha {
+    std::string HexDump(alpha::Slice data);
+}
+
+#endif   /* ----- #ifndef __FORMAT_H__  ----- */
