abf9bd4ab9a649345b72f62abe9a3f3f6a4a2379
Peng Wang <pw2191195@gmail.com>
2016-01-16 22:43:33 +0800

    调整文件命名

---
+++ alpha/LoggerFile.cc
@@ -0,0 +1,84 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  LoggerFile.h
+ *        Created:  03/29/15 17:39:54
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * ==============================================================================
+ */
+
+#include "LoggerFile.h"
+#include <limits.h>
+#include <cstdio>
+#include <cstring>
+
+#include "compiler.h"
+#include "logger.h"
+
+namespace alpha {
+LoggerFile::LoggerFile(const std::string& path, const std::string& prog_name,
+                       const std::string& log_level_name)
+    : path_(path), prog_name_(prog_name), log_level_name_(log_level_name) {}
+
+void LoggerFile::Write(const char* content, int len) {
+  MaybeChangeLogFile();
+  if (unlikely(!file_)) {
+    ::fprintf(stderr, content, len);
+  } else {
+    ssize_t n = ::write(file_.fd(), content, len);
+    if (unlikely(n != len)) {
+      ::perror("write trucated");
+    }
+  }
+}
+
+void LoggerFile::MaybeChangeLogFile() {
+  TimeStamp now = Now();
+  if (likely(InSameHour(now, file_create_time_) && file_)) {
+    return;
+  }
+
+  if (likely(!!file_)) {
+    bool ok = file_.Close();
+    assert(ok);
+    (void)ok;
+  }
+
+  struct tm result;
+  const time_t t = now / kMilliSecondsPerSecond;
+  if (unlikely(nullptr == ::localtime_r(&t, &result))) {
+    perror("localtime_r");
+    return;
+  }
+
+  char path[PATH_MAX];
+  ::snprintf(path, sizeof(path), "%s/%s.%4d%02d%02d%02d.%s.log", path_.c_str(),
+             prog_name_.c_str(), result.tm_year + 1900, result.tm_mon + 1,
+             result.tm_mday, result.tm_hour, log_level_name_.c_str());
+  int open_flags = O_WRONLY | O_APPEND | O_CREAT;
+  File new_file(path, open_flags);
+  if (!new_file) {
+    perror("open");
+  } else {
+    file_ = std::move(new_file);
+    UpdateSymLinkFile(strrchr(path, '/') + 1);
+    file_create_time_ = now;
+  }
+}
+
+void LoggerFile::UpdateSymLinkFile(const char* file) {
+  std::string symlink_path = SymLinkPath();
+  unlink(symlink_path.c_str());
+  symlink(file, symlink_path.c_str());
+}
+
+std::string LoggerFile::SymLinkPath() const {
+  char path[PATH_MAX];
+  ::snprintf(path, sizeof(path), "%s/%s.%s.log", path_.c_str(),
+             prog_name_.c_str(), log_level_name_.c_str());
+  return path;
+}
+}
---
+++ alpha/LoggerFile.h
@@ -0,0 +1,40 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  LoggerFile.h
+ *        Created:  03/27/15 21:27:20
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * ==============================================================================
+ */
+
+#pragma once
+
+#include <string>
+#include <alpha/slice.h>
+#include <alpha/time_util.h>
+#include <alpha/File.h>
+
+namespace alpha {
+class Slice;
+class LoggerFile {
+ public:
+  LoggerFile(const std::string& path, const std::string& prog_name,
+             const std::string& log_level_name);
+  void Write(const char* content, int len);
+
+ private:
+  void MaybeChangeLogFile();
+  void UpdateSymLinkFile(const char* file);
+  std::string SymLinkPath() const;
+
+ private:
+  File file_;
+  TimeStamp file_create_time_{0};
+  std::string path_;
+  std::string prog_name_;
+  std::string log_level_name_;
+};
+}
