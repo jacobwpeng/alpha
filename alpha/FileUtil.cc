cde97a42eae881654f8c9c139b65ac6cb480b942
Peng Wang <pw2191195@gmail.com>
2016-01-16 23:43:13 +0800

    增加FileUtil

---
+++ alpha/FileUtil.cc
@@ -0,0 +1,18 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  FileUtil.cc
+ *        Created:  01/16/16 23:22:01
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * =============================================================================
+ */
+
+#include "FileUtil.h"
+#include <unistd.h>
+
+namespace alpha {
+bool DeleteFile(alpha::Slice path) { return unlink(path.data()) == 0; }
+}
---
+++ alpha/FileUtil.h
@@ -0,0 +1,19 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  FileUtil.h
+ *        Created:  01/16/16 23:18:05
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * =============================================================================
+ */
+
+#pragma once
+
+#include <alpha/slice.h>
+
+namespace alpha {
+bool DeleteFile(alpha::Slice path);
+}
