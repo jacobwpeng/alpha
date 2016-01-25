649f5fcb9da753c28b9bb18d8147f13b17b7ba74
Peng Wang <pw2191195@gmail.com>
2016-01-25 14:28:39 +0800

    补遗漏提交

---
+++ alpha/TimeUtil.cc
@@ -0,0 +1,34 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  TimeUtil.cc
+ *        Created:  03/29/15 17:52:29
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * ==============================================================================
+ */
+
+#include "TimeUtil.h"
+#include <chrono>
+
+namespace alpha {
+TimeStamp Now() {
+  auto now = std::chrono::system_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+      now.time_since_epoch()).count();
+}
+
+TimeStamp NowInSeconds() { return Now() / kMilliSecondsPerSecond; }
+
+TimeStamp from_time_t(time_t t) {
+  return static_cast<TimeStamp>(t) * kMilliSecondsPerSecond;
+}
+
+time_t to_time_t(TimeStamp ts) { return ts / kMilliSecondsPerSecond; }
+
+bool InSameHour(TimeStamp lhs, TimeStamp rhs) {
+  return (lhs % kSecondsPerHour) == (rhs % kSecondsPerHour);
+}
+}
---
+++ alpha/TimeUtil.h
@@ -0,0 +1,36 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  TimeUtil.h
+ *        Created:  03/29/15 17:51:43
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * ==============================================================================
+ */
+
+#ifndef __TIME_UTIL_H__
+#define __TIME_UTIL_H__
+
+#include <ctime>
+#include <cstdint>
+
+namespace alpha {
+using TimeStamp = uint64_t;
+static const uint32_t kMilliSecondsPerSecond = 1000;
+static const uint32_t kSecondsPerMinutes = 60;
+static const uint32_t kMinutesPerHour = 60;
+static const uint32_t kHoursPerDay = 24;
+static const uint32_t kSecondsPerHour = kSecondsPerMinutes * kMinutesPerHour;
+static const uint32_t kSecondsPerDay = kSecondsPerHour * kHoursPerDay;
+
+TimeStamp Now();
+TimeStamp NowInSeconds();
+TimeStamp from_time_t(time_t t);
+time_t to_time_t(TimeStamp ts);
+
+bool InSameHour(TimeStamp lhs, TimeStamp rhs);
+}
+
+#endif /* ----- #ifndef __TIME_UTIL_H__  ----- */
