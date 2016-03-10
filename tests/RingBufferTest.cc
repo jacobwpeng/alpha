e54660e956fbc0b507a1215d508293ad654ff09f
Peng Wang <pw2191195@gmail.com>
2016-03-10 10:05:59 +0800

    Add test for alpha::RingBuffer

---
+++ tests/RingBufferTest.cc
@@ -0,0 +1,149 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  RingBufferTest.cc
+ *        Created:  03/09/16 16:34:49
+ *         Author:  Peng Wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:
+ *
+ * =============================================================================
+ */
+
+#include <string>
+#include <memory>
+#include <gtest/gtest.h>
+#include <alpha/RingBuffer.h>
+
+class RingBufferTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() override {
+    buffer_ = alpha::RingBuffer::CreateFrom(underlying_buffer_, kBufferSize);
+    ASSERT_NE(buffer_, nullptr);
+  }
+
+  virtual void TearDown() override {
+    memset(underlying_buffer_, 0x0, kBufferSize);
+    buffer_ = nullptr;
+  }
+
+  static const int kBufferSize = alpha::RingBuffer::kMaxBufferBodyLength * 5;
+  char underlying_buffer_[kBufferSize];
+  std::unique_ptr<alpha::RingBuffer> buffer_;
+};
+
+TEST_F(RingBufferTest, CreateFrom) {
+  ASSERT_GT(alpha::RingBuffer::kMinByteSize, 0);
+  auto buffer =
+      alpha::RingBuffer::CreateFrom(nullptr, alpha::RingBuffer::kMinByteSize);
+  EXPECT_EQ(buffer, nullptr);
+
+  char buf[kBufferSize];
+  ASSERT_GE(sizeof(buf), alpha::RingBuffer::kMinByteSize);
+  buffer =
+      alpha::RingBuffer::CreateFrom(buf, alpha::RingBuffer::kMinByteSize - 1);
+  EXPECT_EQ(buffer, nullptr);
+
+  buffer = alpha::RingBuffer::CreateFrom(buf, alpha::RingBuffer::kMinByteSize);
+  EXPECT_NE(buffer, nullptr);
+}
+
+TEST_F(RingBufferTest, RestoreFrom) {
+  std::string msg = "Long live the queen!";
+  bool ok = buffer_->Push(msg.data(), msg.size());
+  ASSERT_TRUE(ok);
+
+  auto buffer = alpha::RingBuffer::RestoreFrom(underlying_buffer_, kBufferSize);
+  EXPECT_NE(buffer, nullptr);
+  int len;
+  char* data = buffer_->Pop(&len);
+  EXPECT_NE(data, nullptr);
+  EXPECT_EQ((size_t)len, msg.size());
+  EXPECT_EQ(memcmp(msg.data(), data, len), 0);
+}
+
+TEST_F(RingBufferTest, Push) {
+  bool ok = buffer_->Push(nullptr, 1024);
+  EXPECT_FALSE(ok);
+  ok = buffer_->Push(underlying_buffer_, 0);
+  EXPECT_FALSE(ok);
+  std::vector<char> buf(alpha::RingBuffer::kMaxBufferBodyLength + 1, 0x3f);
+  ok = buffer_->Push(buf.data(), buf.size());
+  EXPECT_FALSE(ok);
+  ok = buffer_->Push(buf.data(), alpha::RingBuffer::kMaxBufferBodyLength);
+  EXPECT_TRUE(ok);
+  std::string msg = "Long live the queen!";
+  ok = buffer_->Push(msg.data(), msg.size());
+  EXPECT_TRUE(ok);
+}
+
+TEST_F(RingBufferTest, Pop) {
+  int len = -1;
+  char* data = buffer_->Pop(&len);
+  EXPECT_EQ(data, nullptr);
+  EXPECT_EQ(len, 0);
+
+  std::string msg1 = "Long live the queen!";
+  std::string msg2 = "The quick brown fox jumps over the lazy dog";
+
+  bool ok = buffer_->Push(msg1.data(), msg1.size());
+  ASSERT_TRUE(ok);
+  ok = buffer_->Push(msg2.data(), msg2.size());
+  ASSERT_TRUE(ok);
+
+  data = buffer_->Pop(&len);
+  EXPECT_EQ((size_t)len, msg1.size());
+  EXPECT_EQ(memcmp(data, msg1.data(), len), 0);
+
+  char* data2 = buffer_->Pop(&len);
+  EXPECT_EQ((size_t)len, msg2.size());
+  EXPECT_EQ(memcmp(data2, msg2.data(), len), 0);
+
+  // Pop的结果有效周期只能到下一次Pop操作/RingBuffer析构
+  EXPECT_NE(memcmp(data, msg1.data(), msg1.size()), 0);
+
+  data = buffer_->Pop(&len);
+  EXPECT_EQ(data, nullptr);
+  EXPECT_EQ(len, 0);
+}
+
+TEST_F(RingBufferTest, empty) {
+  EXPECT_TRUE(buffer_->empty());
+  std::string msg = "Long live the queen!";
+  buffer_->Push(msg.data(), msg.size());
+  EXPECT_FALSE(buffer_->empty());
+  int len;
+  buffer_->Pop(&len);
+  EXPECT_TRUE(buffer_->empty());
+}
+
+TEST_F(RingBufferTest, TestExtraSpace) {
+  // 通过ExtraSpace来区分队列满和队列空两种情况
+  EXPECT_TRUE(buffer_->empty());
+  std::vector<char> buf(alpha::RingBuffer::kMaxBufferBodyLength, 0x3f);
+  while (buffer_->SpaceLeft()) {
+    int sz = std::min<int>(buf.size(), buffer_->SpaceLeft());
+    buffer_->Push(buf.data(), sz);
+  }
+
+  EXPECT_EQ(buffer_->SpaceLeft(), 0);
+  EXPECT_FALSE(buffer_->empty());
+}
+
+TEST_F(RingBufferTest, TestFulfil) {
+  // 某版本Push时计算SpaceLeft没有把int32_t的长度前缀考虑在内
+  // 导致后续Push时覆盖了之前Push的内容
+  char c = 0x3f;
+  int num = 0;
+  while (buffer_->Push(&c, 1)) {
+    ++num;
+  }
+
+  int len;
+  while (char* data = buffer_->Pop(&len)) {
+    EXPECT_EQ(*data, c);
+    --num;
+  }
+
+  EXPECT_EQ(num, 0);
+}
